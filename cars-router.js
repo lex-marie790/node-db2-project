c11f8ce91b57d82a207f4ea422cf1763c44233af
lex-marie790 <lexi.marie790@gmail.com>
2020-07-24 21:38:26 -0700

    SPRINT - Update & Delete

--- cars-router.js
+++ cars-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./data/db-config.js');
+const { count } = require('./data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -28,6 +29,37 @@ router.post('/', (req, res) => {
     .insert(carsData)
     .then(res.status(201).json({ data:carsData }))
     .catch(err => console.log('Error creating car post'));
+});
+
+router.put('/:id', (req, res) => {
+    const { id } =req.params;
+    const changes = req.body;
+    db('cars')
+    .where('id', id)
+    .update(changes)
+    .then(count => {
+        if(count > 0) {
+            res.status(201).json({ data: count})
+        } else {
+            res.status(404).json('Error creating car post')
+        } 
+    })
+    .catch(err => console.log(err))
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    db('cars')
+    .where('id', id)
+    .del()
+    .then(count => {
+        if(count > 0) {
+            res.status(200).json({ data:count })
+        } else {
+            res.status(404).json('Error deleting car')
+        }
+    })
+    .catch(err => console.log(err))
 });
 
 module.exports = router;
